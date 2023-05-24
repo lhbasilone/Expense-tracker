df8f7a0f4a1bdaf1e8378628b5ace0011e0cc8f4
lhbasilone <lucasbasilone@gmail.com>
2023-05-24 03:25:19 -0300

    TrackerExpense

--- src/components/TransactionForm.jsx
+++ src/components/TransactionForm.jsx
@@ -30,7 +30,7 @@ const TransactionForm = () => {
 
         <input type='number' step="0.01" placeholder='0.00'
           onChange={(e) => setAmount(e.target.value)}
-          className='bg-zinc-600 text-white px-3 py-2 rounded-lg block mb-2 w-full'
+          className='focus:outline-none bg-zinc-600 text-white px-3 py-2 rounded-lg block mb-2 w-full'
           value={amount}
         />
         <button className='bg-indigo-700 hover:bg-indigo-800 active:bg-indigo-950  text-white px-3 py-2 rounded-lg block mb-2 w-full '>
--- src/components/TransactionsItem.jsx
+++ src/components/TransactionsItem.jsx
@@ -6,10 +6,10 @@ export const TransactionsItem = ({transaction}) => {
   return (
     <li className='bg-zinc-600 text-white px-3 py-1 rounded-lg mb-2 w-full flex justify-between items-center'>
     <p className='text-sm'>{transaction.description}</p>
-    <div>
-        <span>${transaction.amount}</span>
-        <button class="ml-auto bg-red-600 hover:bg-red-700 active:bg-red-800 text-white w-4 h-5 rounded-md flex items-center justify-center" onClick={() => {deleteTransaction(transaction.id);}}>X</button>
-    </div>
+    <div className="flex items-center">
+  <span>${transaction.amount}</span>
+  <button className="ml-2 bg-red-600 hover:bg-red-700 active:bg-red-800 text-white w-4 h-5 rounded-md flex items-center justify-center" onClick={() => {deleteTransaction(transaction.id);}}>X</button>
+</div>
 </li>
   )
 }
